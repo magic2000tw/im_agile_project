2308703321c9fa938abad7e6a5adcf738cc59ef7
LAPTOP-44S3NG6I\kan <s104213059@mail1.ncnu.edu.tw>
2017-12-20 00:47:49 +0800

    login and username can be used

--- www/kan/test_login.php
+++ www/kan/test_login.php
@@ -3,7 +3,7 @@ session_start();
 $_SESSION['sID'] = 0;
 ?>
 <h1>Login test</h1><hr />
-<form method="post" action="https://localhost/agile/loginModule/loginControl.php">
+<form method="post" action="https://localhost/pro/loginControl.php">
 <input type="hidden" name="act" value="login">
 	loginMail: <input type="text" name="loginMail"><br />
 	Password : <input type="password" name="password"><br />
