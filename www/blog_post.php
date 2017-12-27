7e10ea71a13b43e8a76bec67b0211133ad5607f3
LAPTOP-44S3NG6I\kan <s104213059@mail1.ncnu.edu.tw>
2017-12-27 19:34:08 +0800

    okk

---
+++ www/blog_post.php
@@ -0,0 +1,147 @@
+<?php include("dbconnect.php");?>
+
+<?php 
+$id = $_GET['Id'];//接收Id
+
+$query_blog = "SELECT * FROM blog WHERE Id = $id";
+$sql = mysqli_query($conn,$query_blog) or die(mysqli_error());
+$row = mysqli_fetch_assoc($sql);
+
+// $sql = mysql_query("SELECT * FROM `blog` WHERE `Id`=$id");
+// $row = mysql_fetch_array($sql);
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>遊記文章 - <?php echo $row['Title'];?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <link href="css/body.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Pangolin" rel="stylesheet">
+    <style>
+        body{
+            font-family: 'Pangolin', cursive;
+        }
+
+        #bodydiv{
+          padding-left: 50px;
+        }
+    </style>
+</head>
+<body style="background-color:#ebebeb">
+<div id="wrapper">
+        <div id="sidebar-wrapper" style="background-color:white">
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-12">
+                      <div class="card mt-4" style="background-color:#5b88fc">
+                          
+                              <div class="card-body">
+                                  
+                                  <img src="img/user1.png" width="80%" style="display: block;margin:0 auto;">
+                              </div>
+                      
+                              <div class="card-footer d-flex justify-content-center"style="background-color:gray;">
+                                  <small style="color:white;">賬戶名</small>
+                              </div>      
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="container mt-4">
+            <div class="card mt-4">
+              <a class="btn text-left" style="color:white;background-color:#5b88fc"href="home.html">我的行程</a>
+            </div>
+            <div class="card mt-1">
+                <a class="btn text-left" style="color:white;background-color:#5b88fc"href="love.html">收藏景點</a>
+              </div>
+              <div class="card mt-1">
+                <a class="btn text-left" style="color:white;background-color:#5b88fc"href="blog.php">遊記專區</a>
+              </div>
+              <div class="card mt-1">
+                <a class="btn text-left" style="color:white;background-color:#5b88fc"href="blog_admin.php">遊記後台管理</a>
+              </div>
+                <div class="card mt-1">
+                <a class="btn text-left" style="color:white;background-color:#5b88fc"href="login.html">登出</a>
+              </div>
+          </div>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <nav class="navbar navbar-light " style="background-color:#5b88fc">
+            <a href="#menu-toggle" class="btn btn-sm btn-secondary" id="menu-toggle"><img src="img/menu.png" vertical-align="center" height="20px"></a>
+            <div class="container" >
+
+                <div class="navbar-header">
+                
+                    <div>
+                        <a><img class="img-responsive"src="img/logo.png" height="80px"align="center"></a>
+                        <a><img class="img-responsive"src="img/name3.png" height="50px" align="center"></a>
+                    </div>
+                </div>
+                <div class="input-group col-lg-4">
+                    <input type="search" class="form-control" placeholder="搜尋">
+                    <span class="input-group-btn">
+                        <button class="btn btn-secondary" type="button"><img src="img/search.png" height="30px"></button>
+                    </span>
+                </div>
+            </div>
+        </nav>
+      <div id="bodydiv" alt="內容">
+        <div id="blog" class="section container blog-classic">
+          <div class="row">
+            <div class="col-md-8 mb-sm-160">
+              
+                <!-- Blog Post -->
+                <div class="col-md-12 blog-post" data-wow-delay=".1s" data-wow-duration="2s">
+
+                  <!-- Image -->
+                  <img class="post-img" src="img/<?php echo $row['Pic'];?>.jpg" alt="image">
+
+                  <!-- Meta data -->
+                  <div class="post-meta">
+                      <span><?php echo $row['Date'];?></span>
+                  </div><!-- / .meta -->
+                  <!-- Title -->
+                  <h2 class="post-title"><?php echo $row['Title'];?></h2>
+                  <div class="content-post">
+                    <!-- Intro -->
+                    <?php echo $row['Content'];?>
+                  </div>
+                  
+                </div><!-- / .blog-post-single -->
+            </div>
+    </div>
+</div>
+<footer class="py-5 bg-dark">
+    <div>
+        <p class="m-0 text-center text-white">Thank you</p>
+    </div>
+</footer>
+    <!-- /#wrapper -->
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+</body>
+</html>
---
+++ www/login.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+$_SESSION['sID'] = 0;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>登入</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Pangolin" rel="stylesheet"><style>
+        body{
+        font-family: 'Pangolin', cursive;
+        }
+    </style> -->
+    <link href="https://fonts.googleapis.com/css?family=Pangolin" rel="stylesheet">
+    <style>
+
+        body{
+            font-family: 'Pangolin', cursive;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        
+        <!-- Page Content -->
+        <nav class="navbar navbar-light " style="background-color:#5b88fc">
+            <div class="container">
+                <div class="navbar-brand">
+                <!-- Logo -->
+                    <div>
+                        <a><img class="img-responsive"src="logo.png" width="15%" align="center"></a>
+                        <a><img class="img-responsive"src="name3.png" width="50%" align="center"></a>
+                    </div>
+                
+                </div><!-- / .navbar-header -->
+                
+            </div>
+        </nav>
+        <div class="container col-md-6">
+            <div class="card mt-4 mb-4" style="margin:0 auto;">
+                <div class="card-header" style="background-color:#ebebeb">登入</div>
+                <div class="card-body">
+                    <form method="post" action="loginControl.php">
+                        <input type="hidden" name="act" value="login">
+                        <div class="form-group">
+                            <label>用戶名/電郵</label>
+                            <input type="text" class="form-control" name="loginMail"placeholder="輸入用戶名或電郵">
+                        </div>
+                        <div class="form-group">
+                            <label>帳密</label>
+                            <input type="password" class="form-control" name="password" placeholder="輸入密碼">
+                        </div>
+                        <button type="submit" class="btn col-md-2" >登入</button>
+                        <button type="button" class="btn col-md-2" onclick="window.location='signup.html'">註冊</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+
+</body>
+
+</html>
