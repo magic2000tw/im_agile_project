f184f3f0ae2f180a4ac789574f1fc8a60f7b35f0
Unknown <s104213059@mail1.ncnu.edu.tw>
2018-01-08 21:51:44 +0800

    index php added and set hot.php as home page, change home.php name to myTrip.php

--- www/kan/loginControl.php
+++ www/kan/loginControl.php
@@ -11,12 +11,12 @@ case 'login':
 	if ($id = checkUP($loginMail,$passWord) ) {
 		$_SESSION['userid'] = $id;
 		//$_SESSION['username']=$row['userName'];
-		header('Location:home.php');
+		header('Location:hot.php');
 		// echo "Login OK <br />";
 		// echo '<a href="home.html">VIEW</a> ';
 	} else {
 		echo "Invalid Username or Password - Please try again <br />";
-		echo '<a href="test_login.php">Login again</a> ';
+		echo '<a href="login.php">Login again</a> ';
 	}
 	break;
 }
