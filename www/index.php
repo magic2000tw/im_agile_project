df28acf09adfd0debe9c79b165baae1cee31e87f
Unknown <s104213059@mail1.ncnu.edu.tw>
2018-01-10 14:22:49 +0800

    correct index.php

--- www/index.php
+++ www/index.php
@@ -1,9 +1,9 @@
 <?php
   if ( ! isset($_SESSION['uID']) or $_SESSION['uID'] <= 0) {
-    header("Location: ../kan/login.php");
+    header("Location: ../www/login.php");
     exit(0);
   }else {
-  	header("Location: ../kan/hot.php");
+  	header("Location: ../www/hot.php");
     exit(0);
   }
 ?>
