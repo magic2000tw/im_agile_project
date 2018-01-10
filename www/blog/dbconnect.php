475b9576eb06205d072edf54c10d292f80943122
monkey <monkey50289@gmail.com>
2018-01-10 16:30:51 +0800

    Signed-off-by: monkey <monkey50289@gmail.com>

--- www/blog/dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'localhost';
-$user = 'root';
-$pass = '123456789';
-$db = 'agile';
-$conn = mysqli_connect($host, $user, $pass, $db) or die('Error with MySQL connection');
-mysqli_query($conn,"SET NAMES utf8"); 
-?>
